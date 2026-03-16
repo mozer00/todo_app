77ce30b1befcc86738a08ff68b4a63058390f2b3
Matheus Mozer <mozer.prog@gmail.com>
2026-03-16 06:13:10 -0300

    feat: project structure and database configuration

---
+++ app/core/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker, DeclarativeBase
+
+# conexao com o banco de dados
+DATABASE_URL = "sqlite:///./todo.db"
+
+# fastapi usa multi threads, sqlite só permite uso na thread que criou, entao tem que tratar
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+#autocommit/autoflush=false para controlar quando salvar e para o sqlalchemy nao enviar sql pro banco antes do commit
+SessionLocal = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+
+#classe base, todo model vai herdar dela
+class Base(DeclarativeBase):
+    pass
+
+#abre e fecha a conexao com o banco de dados, usando o yield para criar um gerenciador de contexto
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
